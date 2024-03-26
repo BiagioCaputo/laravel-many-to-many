1d62668d9ab52f3d4c0b1663dc533bf7fa661475
BiagioCaputo <scebiagio98@gmail.com>
2024-03-26 15:28:29 +0100

    table pivot project_technology created and implemented function many to many between projects and technologies

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -38,4 +38,9 @@ class Project extends Model
     {
         return $this->belongsTo(Type::class);
     }
+
+    public function technologies()
+    {
+        return $this->belongstoMany(Technology::class);
+    }
 }
