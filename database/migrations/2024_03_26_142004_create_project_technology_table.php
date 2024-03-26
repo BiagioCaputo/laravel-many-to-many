dd3365c8404745a8eaa13fb5403d1bb37d428364
BiagioCaputo <scebiagio98@gmail.com>
2024-03-26 15:35:44 +0100

    added missing columns in the pivot table

--- database/migrations/2024_03_26_142004_create_project_technology_table.php
+++ database/migrations/2024_03_26_142004_create_project_technology_table.php
@@ -3,6 +3,8 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Project;
+use App\Models\Technology;
 
 return new class extends Migration
 {
@@ -13,7 +15,9 @@ return new class extends Migration
     {
         Schema::create('project_technology', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->foreignIdFor(Project::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Technology::class)->constrained()->cascadeOnDelete();
+
         });
     }
 
